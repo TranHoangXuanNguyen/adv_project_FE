3d5965415080888c8836f04bc7cba0b89af99568
Xabe14 <bsa30012@gmail.com>
2025-05-09 23:52:20 +0700

    done UI card weeklist and weekinfor

--- src/pages/student/CardList.jsx
+++ src/pages/student/CardList.jsx
@@ -1,55 +1,62 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
   faUser,
   faBullseye,
   faBook,
-  faFolder
-} from '@fortawesome/free-solid-svg-icons';
+  faFolder,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ isAddCard, title, startDate, endDate, goals, onAddCard }) => {
-    if (isAddCard) {
-      return (
-        <div
-          className="bg-pink-200 rounded-2xl p-4 shadow-md flex justify-center items-center text-4xl cursor-pointer w-72 h-48"
-          onClick={onAddCard}
-        >
-          <span>+</span>
-        </div>
-      );
-    }
-  
+  if (isAddCard) {
     return (
-      <Link
-  to={`/weekinfo/${title}`}
-  onClick={() => {
-    localStorage.setItem('selectedCard', JSON.stringify({ title, startDate, endDate, goals }));
-  }}
->
-  <div className="bg-pink-200 rounded-2xl py-1 px-3 shadow-md w-72 h-48 flex flex-col justify-between">
-    <h3 className="text-lg font-semibold p-0">{title}</h3>
-    <hr className="border-t border-black opacity-20" />
-    <p className="text-sm pt-2">Start day: {startDate}</p>
-    <p className="text-sm pb-4">End day: {endDate}</p>
-    <hr className="border-t border-black opacity-20" />
-    <h4 className="text-md font-semibold">Goals</h4>
-    <div className="flex flex-col space-y-2">
-      {goals.slice(0, 2).map((goal, index) => (
-        <span key={index} className="text-md">{`${index + 1}. ${goal}`}</span>
-      ))}
-    </div>
-  </div>
-</Link>
+      <div
+        className="bg-[#fdefee] rounded-2xl p-4 shadow-md w-72 h-48 relative cursor-pointer flex justify-center items-center"
+        onClick={onAddCard}
+      >
+        <div className="absolute w-16 h-16 bg-white rounded-full flex justify-center items-center">
+          <span className="text-4xl text-#9a7677]">+</span>
+        </div>
+      </div>
     );
-  };
+  }
+
+  return (
+    <Link
+      to={`/weekinfo/${title}`}
+      onClick={() => {
+        localStorage.setItem(
+          "selectedCard",
+          JSON.stringify({ title, startDate, endDate, goals })
+        );
+      }}
+    >
+      <div className="bg-[#fdefee] rounded-2xl py-1 px-3 shadow-md w-72 h-48 flex flex-col justify-between">
+        <h3 className="text-lg font-semibold p-0">{title}</h3>
+        <hr className="border-t border-black opacity-20" />
+        <p className="text-md pt-2">Start day: {startDate}</p>                              
+        <p className="text-md pb-4">End day: {endDate}</p>
+        <hr className="border-t border-black opacity-20" />
+        <h4 className="text-md font-semibold">Goals</h4>
+        <div className="flex flex-col space-y-2">
+          {goals.slice(0, 2).map((goal, index) => (
+            <span key={index} className="text-md">{`${
+              index + 1
+            }. ${goal}`}</span>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 const CardFormModal = ({ onAddNewCard, onCancel }) => {
-  const [title, setTitle] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [goalInput, setGoalInput] = useState('');
+  const [title, setTitle] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [goalInput, setGoalInput] = useState("");
   const [goals, setGoals] = useState([]);
 
   const handleGoalChange = (e) => {
@@ -57,9 +64,9 @@ const CardFormModal = ({ onAddNewCard, onCancel }) => {
   };
 
   const handleAddGoal = () => {
-    if (goalInput.trim() !== '') {
+    if (goalInput.trim() !== "") {
       setGoals([...goals, goalInput]);
-      setGoalInput('');
+      setGoalInput("");
     }
   };
 
@@ -74,7 +81,10 @@ const CardFormModal = ({ onAddNewCard, onCancel }) => {
     <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50">
       <div className="bg-white p-6 rounded-lg shadow-md w-[734px]">
         <h2 className="text-lg font-semibold mb-4">Add New Week</h2>
-        <form onSubmit={handleSubmit} className="h-full flex flex-col justify-between">
+        <form
+          onSubmit={handleSubmit}
+          className="h-full flex flex-col justify-between"
+        >
           <div className="mb-4">
             <label className="block text-sm font-medium mb-2">Title</label>
             <input
@@ -122,15 +132,18 @@ const CardFormModal = ({ onAddNewCard, onCancel }) => {
                 <button
                   type="button"
                   onClick={handleAddGoal}
-                  className="bg-blue-500 text-white px-3 rounded"
+                  className="bg-blue-500 text-white px-3 py-2 rounded"
                 >
-                  Add Goal
+                  +
                 </button>
               </div>
             </div>
           </div>
           <div className="flex justify-between">
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+            >
               Add
             </button>
             <button
@@ -157,7 +170,7 @@ const CardList = () => {
   };
 
   return (
-    <div className="flex flex-wrap gap-5">
+    <div className=" py-5 pl-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
       <Card isAddCard onAddCard={() => setShowModal(true)} />
       {cards.map((card, index) => (
         <Card
@@ -181,7 +194,11 @@ const Sidebar = () => (
   <div className="w-[250px] bg-gradient-to-b from-[#00aaff] to-[#0077cc] text-white p-5 flex flex-col">
     <div className="flex items-center mb-5">
       <FontAwesomeIcon icon={faBars} className="text-xl mr-2" />
-      <img src="..src/assets/img/pnlogo.png" alt="Logo" className="w-10 h-10 rounded-full mr-2" />
+      <img
+        src="..src/assets/img/pnlogo.png"
+        alt="Logo"
+        className="w-10 h-10 rounded-full mr-2"
+      />
       <h3 className="text-2xl font-semibold">JOURNAL</h3>
     </div>
     <hr className="border border-white/50 my-3" />
@@ -209,12 +226,11 @@ const WeekList = () => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 p-8 bg-gray-100 flex justify-center">
+      <div className="flex-1 p-8 bg-gray-100 flex">
         <CardList />
       </div>
     </div>
   );
 };
-
 
 export default WeekList;
--- src/routes/studentRoutes.jsx
+++ src/routes/studentRoutes.jsx
@@ -1,6 +1,5 @@
 // import PublicLayout from '../layouts/PublicLayout';
 import Home from '../pages/Home';
-import About from '../pages/About';
 import WeekInfor from '../pages/student/WeekInfor';
 import WeekList from '../pages/student/CardList';
 import Goal from '../pages/student/GoalTable';
@@ -10,7 +9,6 @@ const studentRoutes = {
 //   element: <PublicLayout />,
   children: [
     { index: true, element: <Home /> },
-    { path: 'about', element: <About /> },
     { path:"weekinfo/:id", element:<WeekInfor /> },
     { path: 'weeklist', element: <WeekList /> },
     { path: 'Goal', element: <Goal /> },
