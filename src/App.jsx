1bf6ab0a63d5b1da09339c3dec06b27636759000
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-23 17:10:51 +0700

    done send notify

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,31 @@ import routes from "./routes";
 import { onMessage } from "firebase/messaging";
 import { messaging } from "./services/FireBaseConfig";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App() {
   useEffect(() => {
     const unsubscribe = onMessage(messaging, (payload) => {
       console.log("Thông báo đến khi đang mở:", payload);
+
+      const { title, body } = payload.notification || {};
+      toast.info(
+        `${title ? title + ": " : ""}${body || "Bạn có thông báo mới"}`
+      );
     });
+
     return () => {
       unsubscribe?.();
     };
   }, []);
 
   const element = useRoutes(routes);
-  return element;
+
+  return (
+    <>
+      {element}
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar />
+    </>
+  );
 }
